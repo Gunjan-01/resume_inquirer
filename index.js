e9e86ee9b6cdfcf44de884c3e3ed7dc6427fd1d0
1gunju12345 <gunjanyadav048@gmail.com>
2020-09-03 18:55:29 +0530

    4

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 'use strict';
 
 var inquirer = require('inquirer');
@@ -8,45 +7,46 @@ var response = chalk.bold.green;
 var resume = require('./resume.json');
 
 var resumePrompts = {
-  type: 'list',
-  name: 'resumeOptions',
-  message: 'What do you want to know about me?',
-  choices: [...Object.keys(resume), 'Exit'],
+    type: 'list',
+    name: 'resumeOptions',
+    message: 'What do you want to know about me?',
+    choices: [
+        ...Object.keys(resume),
+        'Exit'
+    ]
 };
 
 function main() {
-  console.log('Hey,My name is Gunjan and welcome to my resume');
-  resumeHandler();
+    console.log('Hey,My name is Gunjan and welcome to my resume');
+    resumeHandler();
 }
 
 function resumeHandler() {
-  inquirer.prompt(resumePrompts).then((ans) => {
-    if (ans.resumeOptions == 'Exit') {
-      return;
-    }
-    var option = ans.resumeOptions;
-    console.log(response('--------------------------------------'));
-    resume[`${option}`].forEach((info) => {
-      console.log(response('|   => ' + info));
-    });
-    console.log(response('--------------------------------------'));
-
-    inquirer
-      .prompt({
-        type: 'list',
-        name: 'exitBack',
-        message: 'Go back or Exit?',
-        choices: ['Back', 'Exit'],
-      })
-      .then((choice) => {
-        if (choice.exitBack == 'Back') {
-          console.clear();
-          resumeHandler();
-        } else {
-          return;
+    inquirer.prompt(resumePrompts).then((ans) => {
+        if (ans.resumeOptions == 'Exit') {
+            return;
         }
-      });
-  });
+        var option = ans.resumeOptions;
+        console.log(response('--------------------------------------'));
+        resume[`${option}`].forEach((info) => {
+            console.log(response('|   => ' + info));
+        });
+        console.log(response('--------------------------------------'));
+
+        inquirer.prompt({
+            type: 'list',
+            name: 'exitBack',
+            message: 'Go back or Exit?',
+            choices: ['Back', 'Exit']
+        }).then((choice) => {
+            if (choice.exitBack == 'Back') {
+                console.clear();
+                resumeHandler();
+            } else {
+                return;
+            }
+        });
+    });
 }
 
 main();
